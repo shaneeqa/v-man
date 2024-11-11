4221c015d61810cd024a21e6a4691f132da755a9
shaneeqa <aneeqahmadh@gmail.com>
2024-11-12 01:26:11 +0530

    [vman-39] added annotations in ApiDocConfig file

--- src/main/java/com/aneeq/venuemanager/config/ApiDocConfig.java
+++ src/main/java/com/aneeq/venuemanager/config/ApiDocConfig.java
@@ -4,8 +4,13 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ApiDocConfig {
+
+    @Bean
     public OpenAPI config() {
         return new OpenAPI()
                 .info(new Info()
